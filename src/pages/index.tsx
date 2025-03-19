e1d18f6af1455d374388876cbd7eecec5de35a81
gyeongyeon <gjeon0321@gmail.com>
2025-03-20 02:01:19 +0900

    fix: 블로그 글 노출 순서 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,15 +59,17 @@ export default function Home() {
             </div>
           }
         >
-          {processedData?.map((post, index) => (
-            <Card
-              key={index}
-              id={post.postId}
-              title={post.title}
-              description={post.des}
-              src={post.src}
-            />
-          ))}
+          {processedData
+            ?.reverse()
+            .map((post, index) => (
+              <Card
+                key={index}
+                id={post.postId}
+                title={post.title}
+                description={post.des}
+                src={post.src}
+              />
+            ))}
         </Drawer>
       </div>
     </div>
